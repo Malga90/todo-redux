9e50805985f872a1f8506c4907b352385fa34002
Malga90 <savemeebarry@gmail.com>
2019-09-05 10:26:59 +0200

    styling todo list with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -3,27 +3,52 @@ import "./App.css";
 import ListGroup from "react-bootstrap/ListGroup";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 function App() {
   return (
     <div className="App">
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
-        />
-        <InputGroup.Append>
-          <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
-        </InputGroup.Append>
-      </InputGroup>
-      <ListGroup>
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
+      <Container>
+        <Row className="justify-content--center">
+          <h1>To do list - Redux </h1>
+        </Row>
+
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="What would you like to do?"
+            aria-label="To do to add"
+            aria-describedby="basic-addon2"
+          />
+          <InputGroup.Append>
+            <Button variant="info">Add</Button>
+          </InputGroup.Append>
+        </InputGroup>
+        <ListGroup>
+          <ListGroup.Item>
+            Get a job
+            <Button variant="success" className="justify-content--end">
+              Done!
+            </Button>
+            <Button variant="danger" className="justify-content--end">
+              Remove
+            </Button>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Learn to surf <Button variant="success">Done!</Button>
+            <Button variant="danger">Remove</Button>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Be healthy <Button variant="success">Done!</Button>
+            <Button variant="danger">Remove</Button>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Get driver's licence <Button variant="success">Done!</Button>
+            <Button variant="danger">Remove</Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </Container>
     </div>
   );
 }
