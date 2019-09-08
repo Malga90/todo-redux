eed316aa3fe6cfd0d122aa62b49a5edc029fdd3a
Malga90 <savemeebarry@gmail.com>
2019-09-08 21:48:04 +0200

    todo list in a seperate file

--- src/App.js
+++ src/App.js
@@ -1,63 +1,10 @@
 import React from "react";
-import "./App.css";
-import ListGroup from "react-bootstrap/ListGroup";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
+import TodoList from "./TodoList";
 
 function App() {
   return (
     <div className="App">
-      <Container className="justify-content-md-center">
-        <Row className="justify-content-md-center">
-          <h1>To do list - Redux </h1>
-        </Row>
-
-        <InputGroup className="mb-3">
-          <FormControl
-            placeholder="What would you like to do?"
-            aria-label="To do to add"
-            aria-describedby="basic-addon2"
-          />
-          <InputGroup.Append>
-            <Button variant="info">Add</Button>
-          </InputGroup.Append>
-        </InputGroup>
-
-        <ListGroup>
-          <ListGroup.Item className="d-flex justify-content-between">
-            Get a job
-            <ButtonGroup>
-              <Button variant="success">Done!</Button>
-              <Button variant="danger">Remove</Button>
-            </ButtonGroup>
-          </ListGroup.Item>
-          <ListGroup.Item className="d-flex justify-content-between">
-            Learn to surf
-            <ButtonGroup>
-              <Button variant="success">Done!</Button>
-              <Button variant="danger">Remove</Button>
-            </ButtonGroup>
-          </ListGroup.Item>
-          <ListGroup.Item className="d-flex justify-content-between">
-            Be healthy
-            <ButtonGroup>
-              <Button variant="success">Done!</Button>
-              <Button variant="danger">Remove</Button>
-            </ButtonGroup>
-          </ListGroup.Item>
-          <ListGroup.Item className="d-flex justify-content-between">
-            Get driver's licence
-            <ButtonGroup>
-              <Button variant="success">Done!</Button>
-              <Button variant="danger">Remove</Button>
-            </ButtonGroup>
-          </ListGroup.Item>
-        </ListGroup>
-      </Container>
+      <TodoList />
     </div>
   );
 }
